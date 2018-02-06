d6b6717b88e8f3019b75b75a2cc0026b5f30b7b6
vishalcoderathore <vshl.rathore87@gmail.com>
2018-02-06 10:45:27 -0600

    Introduction to ThymeLeaf

--- src/main/java/com/springboot/springwebappapplication/model/Book.java
+++ src/main/java/com/springboot/springwebappapplication/model/Book.java
@@ -51,6 +51,14 @@ public class Book {
 		return title;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
