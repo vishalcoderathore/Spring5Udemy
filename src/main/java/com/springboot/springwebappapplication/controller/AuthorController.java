8525529a7c55fd6dd1849c2587e2f0220dab1cd4
vishalcoderathore <vshl.rathore87@gmail.com>
2018-02-06 10:58:56 -0600

    Added Authors.html using ThymeLeaf with Author Controller

---
+++ src/main/java/com/springboot/springwebappapplication/controller/AuthorController.java
@@ -0,0 +1,22 @@
+package com.springboot.springwebappapplication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springboot.springwebappapplication.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+	public AuthorRepository authorRepository;
+	
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
+	}
+	
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+		model.addAttribute("authors", authorRepository.findAll());
+		return "authors";
+	}
+}
